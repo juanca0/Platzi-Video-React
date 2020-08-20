fc5e6854ad7f3e075a02fa9c1d0c5be841fc096a
Juan Camilo <juanca2.0@hotmail.com>
2020-08-19 21:49:13 -0500

    se agrega el componenete carrucel y sus estilos

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import Header from '../componenets/Header';
 import Search from '../componenets/Search';
+import Categories from '../componenets/Categories';
+import Carrusel from '../componenets/Carrusel';
+import CarruselItem from '../componenets/CarruselItem';
 import '../assets/styles/App.scss';
 
 const App = () => (
   <div className='App'>
     <Header />
     <Search />
+
+    <Categories>
+      <Carrusel>
+        <CarruselItem />
+        <CarruselItem />
+        <CarruselItem />
+      </Carrusel>
+    </Categories>
   </div>
 );
 
