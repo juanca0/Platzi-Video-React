8006630671febd814c9f3c37ee57f47aa8ee4c8f
Juan Camilo <juanca2.0@hotmail.com>
2020-08-19 20:33:13 -0500

    se ponen estilos al header

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Header from '../componenets/Header';
+import '../assets/styles/App.scss';
 
 const App = () => (
-    <div className='App'>
-      <Header />
-    </div>
-)
+  <div className='App'>
+    <Header />
+  </div>
+);
 
-export default App
+export default App;
