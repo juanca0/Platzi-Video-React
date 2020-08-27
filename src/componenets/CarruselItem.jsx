0c077baa0c5aab4a1ca6f84b5bb891f890049d65
Juan Camilo <juanca2.0@hotmail.com>
2020-08-26 22:10:53 -0500

    se utilizan proptypes para tipas los elementos de los componnetes

--- src/componenets/CarruselItem.jsx
+++ src/componenets/CarruselItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import '../assets/styles/components/CarouselItem.scss';
-
 import playIcon from '../assets/static/play-icon.png';
 import plusIcon from '../assets/static/plus-icon.png';
 
@@ -19,5 +19,13 @@ const CarruselItem = ({ cover, title, year, contentRating, duration }) => (
     </div>
   </div>
 );
+
+CarruselItem.propTypes = {
+cover: PropTypes.string,
+title: PropTypes.string,
+year: PropTypes.number,
+contentRating: PropTypes.string,
+duration: PropTypes.number,
+};
 
 export default CarruselItem;
