3b29fe230fb0a29dcba9d192832d228210c8e3e2
Juan Camilo <juanca2.0@hotmail.com>
2020-09-14 21:35:34 -0500

    se agrega el Layout en todas las paginas

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import Header from '../componenets/Header';
 import Search from '../componenets/Search';
 import Categories from '../componenets/Categories';
 import Carrusel from '../componenets/Carrusel';
 import CarruselItem from '../componenets/CarruselItem';
-import Footer from '../componenets/Footer';
 import useInitialState from '../hooks/useInitialState'
 import '../assets/styles/App.scss';
 
@@ -13,8 +11,7 @@ const API = 'http://localhost:3000/initalState';
 const Home = () => {
   const initalState = useInitialState(API);
   return initalState.length === 0 ? <h1>Loading...</h1> : (
-    <div className='App'>
-      <Header />
+    <>
       <Search />
 
       {initalState.mylist.length > 0 && (
@@ -41,8 +38,7 @@ const Home = () => {
         </Carrusel>
       </Categories>
 
-      <Footer />
-    </div>
+    </>
   );
 };
 export default Home;
