dcceeab1ca56e290a5e2cdd6326eb9e1f40ea0c9
Juan Camilo <juanca2.0@hotmail.com>
2020-09-14 21:18:10 -0500

    se crea la pagina NotFound y se ponene estilos

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../assets/styles/components/NotFound.scss';
+
+const NotFound = () => (
+  <>
+    <section className='main'>
+      <div className='animated wobble'>
+        <h1 className='main__title'>404</h1>
+      </div>
+      <p className='main__title--p'>pagina no encontrada</p>
+    </section>
+  </>
+);
+
+export default NotFound;
