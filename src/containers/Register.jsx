e0f578cd9a9ff951342cef9882d4ad6a945c9f72
Juan Camilo <juanca2.0@hotmail.com>
2020-09-22 21:58:33 -0500

    se conecta con redux el register

--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -1,9 +1,11 @@
 /* eslint-disable react/button-has-type */
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { registerRequest } from '../actions';
 import '../assets/styles/components/Register.scss';
 
-const Register = () => {
+const Register = (props) => {
   const [form, setValues] = useState({
     email: '',
     name: '',
@@ -19,7 +21,8 @@ const Register = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(form);
+    props.registerRequest(form);
+    props.history.push('/');
   };
 
   return (
@@ -58,4 +61,8 @@ const Register = () => {
   );
 };
 
-export default Register;
+const mapDispatchToProps = {
+  registerRequest,
+};
+
+export default connect(null, mapDispatchToProps)(Register);
